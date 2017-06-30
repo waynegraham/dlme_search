ed1b5d97a048c73272bf1783056750adf13b3914
Wayne Graham <wgraham@clir.org>
2017-06-30 05:43:32 +0200

    Add base i18n support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   layout 'blacklight'
 
   protect_from_forgery with: :exception
+
+
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
