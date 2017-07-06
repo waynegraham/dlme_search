232aff2d3bc756a3b42d85e1e734bc71703f0009
Mark A. Matienzo <matienzo@stanford.edu>
2017-07-05 17:21:06 -0700

    Implement application profile in Solr/indexing
    * Rewrite default indexing rules to map to application profile definitions
    * Add additional fields where necessary to document handler in solrconfig
    * Update CatalogController and SolrDocument to use new field definitions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def paragraph_separator(args)
+    safe_join(args[:value].map { |paragraph| content_tag(:p, paragraph) })
+  end
+
+  def linebreak_separator(args)
+    safe_join(args[:value], tag(:br))
+  end
+
+  def linkify(args)
+    safe_join(args[:value].map { |v| link_to(v, v) }, tag(:br))
+  end
 end
--- app/models/marc_indexer.rb
+++ app/models/marc_indexer.rb
@@ -13,15 +13,18 @@ class MarcIndexer < Blacklight::Marc::Indexer
       provide 'solr_writer.max_skipped', -1
     end
 
+    # Mapping reference: https://docs.google.com/spreadsheets/d/1BzZvDOf4fgas3TD21xF40lu2pk2XW0k2pTGJKIt6438/edit#gid=1906730566
+
     to_field "id", trim(extract_marc("001"), :first => true)
     to_field 'marc_display', get_xml
     to_field "text", extract_all_marc_values do |r, acc|
       acc.replace [acc.join(' ')] # turn it into a single string
     end
      
-    to_field "language_facet", marc_languages("008[35-37]:041a:041d:")
-    to_field "format", get_format
-    to_field "isbn_t",  extract_marc('020a', :separator=>nil) do |rec, acc|
+    to_field "cho_language_sim", marc_languages("008[35-37]:041a:041d:")
+    to_field "cho_format_sim", get_format
+    to_field "cho_format_tsim", get_format
+    to_field "cho_identifier_tsim",  extract_marc('020a', :separator=>nil) do |rec, acc|
          orig = acc.dup
          acc.map!{|x| StdNum::ISBN.allNormalizedValues(x)}
          acc << orig
@@ -29,25 +32,24 @@ class MarcIndexer < Blacklight::Marc::Indexer
          acc.uniq!
     end
      
-    to_field 'material_type_display', extract_marc('300a', :trim_punctuation => true)
+    to_field 'cho_extent_tsim', extract_marc('300acf', :trim_punctuation => true)
      
     # Title fields
     #    primary title 
      
-    to_field 'title_t', extract_marc('245a')
-    to_field 'title_display', extract_marc('245a', :trim_punctuation => true, :alternate_script=>false)
-    to_field 'title_vern_display', extract_marc('245a', :trim_punctuation => true, :alternate_script=>:only)
+    to_field 'cho_title_tsim', extract_marc('245abnps')
+    to_field 'cho_title_display_tsim', extract_marc('245a', :trim_punctuation => true, :alternate_script=>false)
+    to_field 'cho_title_vern_display_tsim', extract_marc('245a', :trim_punctuation => true, :alternate_script=>:only)
      
     #    subtitle
      
-    to_field 'subtitle_t', extract_marc('245b')
-    to_field 'subtitle_display', extract_marc('245b', :trim_punctuation => true, :alternate_script=>false)
-    to_field 'subtitle_vern_display', extract_marc('245b', :trim_punctuation => true, :alternate_script=>:only)
+    #to_field 'subtitle_t', extract_marc('245b')
+    # to_field 'subtitle_display', extract_marc('245b', :trim_punctuation => true, :alternate_script=>false)
+    # to_field 'subtitle_vern_display', extract_marc('245b', :trim_punctuation => true, :alternate_script=>:only)
      
     #    additional title fields
-    to_field 'title_addl_t', 
+    to_field 'cho_alternate_tsim',
       extract_marc(%W{
-        245abnps
         130#{ATOZ}
         240abcdefgklmnopqrs
         210ab
@@ -56,106 +58,99 @@ class MarcIndexer < Blacklight::Marc::Indexer
         243abcdefgklmnopqrs
         246abcdefgnp
         247abcdefgnp
+        700gklmnoprst
+        710fgklmnopqrst
+        711fgklnpst
+        730abcdefgklmnopqrst
+        740anp
       }.join(':'))
+
+    # Series added entry
+    to_field 'cho_is_part_of_tsim', extract_marc("440anpv:490av")
      
-    to_field 'title_added_entry_t', extract_marc(%W{
-      700gklmnoprst
-      710fgklmnopqrst
-      711fgklnpst
-      730abcdefgklmnopqrst
-      740anp
-    }.join(':'))
+    to_field 'cho_title_sort', marc_sortable_title
      
-    to_field 'title_series_t', extract_marc("440anpv:490av")
+    # Creator/contributor fields
      
-    to_field 'title_sort', marc_sortable_title  
-     
-    # Author fields
-     
-    to_field 'author_t', extract_marc("100abcegqu:110abcdegnu:111acdegjnqu")
-    to_field 'author_addl_t', extract_marc("700abcegqu:710abcdegnu:711acdegjnqu")
-    to_field 'author_display', extract_marc("100abcdq:110#{ATOZ}:111#{ATOZ}", :alternate_script=>false)
-    to_field 'author_vern_display', extract_marc("100abcdq:110#{ATOZ}:111#{ATOZ}", :alternate_script=>:only)
+    to_field 'cho_creator_tsim', extract_marc("100abcegqu:110abcdegnu:111acdegjnqu")
+    to_field 'cho_creator_display_tsim', extract_marc("100abcdq:110#{ATOZ}:111#{ATOZ}", :alternate_script=>false)
+    to_field 'cho_creator_vern_display_tsim', extract_marc("100abcdq:110#{ATOZ}:111#{ATOZ}", :alternate_script=>:only)
+    to_field 'cho_contributor_tsim', extract_marc("700abcegqu:710abcdegnu:711acdegjnqu")
+    to_field 'cho_contributor_display_tsim', extract_marc("700abcdq:710#{ATOZ}:711#{ATOZ}", :alternate_script=>false)
+    to_field 'cho_contributor_vern_display_tsim', extract_marc("700abcdq:710#{ATOZ}:711#{ATOZ}", :alternate_script=>:only)
+    to_field 'author_display_sim', extract_marc("100abcdq:110#{ATOZ}:111#{ATOZ}:700abcdq:710#{ATOZ}:711#{ATOZ}", :alternate_script=>false)
+    to_field 'author_display_tsim', extract_marc("100abcdq:110#{ATOZ}:111#{ATOZ}:700abcdq:710#{ATOZ}:711#{ATOZ}", :alternate_script=>false)
+    to_field 'author_vern_display_tsim', extract_marc("100abcdq:110#{ATOZ}:111#{ATOZ}:700abcdq:710#{ATOZ}:711#{ATOZ}", :alternate_script=>:only)
      
     # JSTOR isn't an author. Try to not use it as one
     to_field 'author_sort', marc_sortable_author
      
     # Subject fields
-    to_field 'subject_t', extract_marc(%W(
+    to_field 'cho_subject_tsim', extract_marc(%W(
       600#{ATOU}
       610#{ATOU}
       611#{ATOU}
       630#{ATOU}
       650abcde
       651ae
-      653a:654abcde:655abc
+      653a:654abcde
     ).join(':'))
-    to_field 'subject_addl_t', extract_marc("600vwxyz:610vwxyz:611vwxyz:630vwxyz:650vwxyz:651vwxyz:654vwxyz:655vwxyz")
+    to_field 'cho_has_type_tsim', extract_marc("600v:610v:611v:630v:650v:651v:654v:655abv")
+    to_field 'cho_has_type_sim', extract_marc("600v:610v:611v:630v:650v:651v:654v:655abv")
     to_field 'subject_topic_facet', extract_marc("600abcdq:610ab:611ab:630aa:650aa:653aa:654ab:655ab", :trim_punctuation => true)
-    to_field 'subject_era_facet',  extract_marc("650y:651y:654y:655y", :trim_punctuation => true)
-    to_field 'subject_geo_facet',  extract_marc("651a:650z",:trim_punctuation => true )
+    to_field 'cho_temporal_sim',  extract_marc("650y:651y:654y:655y", :trim_punctuation => true)
+    to_field 'cho_temporal_tsim',  extract_marc("650y:651y:654y:655y", :trim_punctuation => true)
+    to_field 'cho_spatial_sim',  extract_marc("651a:650z", :trim_punctuation => true )
+    to_field 'cho_spatial_tsim',  extract_marc("651a:650z", :trim_punctuation => true )
      
     # Publication fields
-    to_field 'published_display', extract_marc('260a', :trim_punctuation => true, :alternate_script=>false)
-    to_field 'published_vern_display', extract_marc('260a', :trim_punctuation => true, :alternate_script=>:only)
-    to_field 'pub_date', marc_publication_date
-     
-    # Call Number fields
-    to_field 'lc_callnum_display', extract_marc('050ab', :first => true)
-    to_field 'lc_1letter_facet', extract_marc('050ab', :first=>true, :translation_map=>'callnumber_map') do |rec, acc|
-      # Just get the first letter to send to the translation map
-      acc.map!{|x| x[0]}
-    end
+    to_field 'cho_publisher_tsim', extract_marc('260ab', :trim_punctuation => true)
+    to_field 'cho_publisher_display_tsim', extract_marc('260ab', :trim_punctuation => true, :alternate_script=>false)
+    to_field 'cho_publisher_vern_display_tsim', extract_marc('260ab', :trim_punctuation => true, :alternate_script=>:only)
+    to_field 'cho_date_sim', marc_publication_date
+    to_field 'cho_date_display_tsim', marc_publication_date
 
-    alpha_pat = /\A([A-Z]{1,3})\d.*\Z/
-    to_field 'lc_alpha_facet', extract_marc('050a', :first=>true) do |rec, acc|
-      acc.map! do |x|
-        (m = alpha_pat.match(x)) ? m[1] : nil
+    # Notes
+    to_field 'cho_description_tsim', extract_marc('520:500')
+    to_field 'cho_provenance_tsim', extract_marc('561')
+
+    # Provider links
+    to_field 'agg_data_provider_sim', extract_marc('852ab')
+    to_field 'agg_data_provider_tsim', extract_marc('852ab')
+    to_field 'agg_provider_sim', extract_marc('852a', :trim_punctuation => true, :first=>true)
+    to_field 'agg_provider_tsim', extract_marc('852a', :trim_punctuation => true, :first=>true)
+    to_field('agg_is_shown_at_tsim') do |rec, acc|
+      rec.fields('856').each do |f|
+        case f.indicator2
+        when '1'
+          f.find_all{|sf| sf.code == 'u'}.each do |url|
+            acc << url.value
+          end
+        end
       end
-      acc.compact! # eliminate nils
     end
 
-    to_field 'lc_b4cutter_facet', extract_marc('050a', :first=>true)
+    # # Call Number fields
+    # to_field 'lc_callnum_display', extract_marc('050ab', :first => true)
+    # to_field 'lc_1letter_facet', extract_marc('050ab', :first=>true, :translation_map=>'callnumber_map') do |rec, acc|
+    #   # Just get the first letter to send to the translation map
+    #   acc.map!{|x| x[0]}
+    # end
+    #
+    # alpha_pat = /\A([A-Z]{1,3})\d.*\Z/
+    # to_field 'lc_alpha_facet', extract_marc('050a', :first=>true) do |rec, acc|
+    #   acc.map! do |x|
+    #     (m = alpha_pat.match(x)) ? m[1] : nil
+    #   end
+    #   acc.compact! # eliminate nils
+    # end
+
+    # to_field 'lc_b4cutter_facet', extract_marc('050a', :first=>true)
      
     # URL Fields
      
-    notfulltext = /abstract|description|sample text|table of contents|/i
-     
-    to_field('url_fulltext_display') do |rec, acc|
-      rec.fields('856').each do |f|
-        case f.indicator2
-        when '0'
-          f.find_all{|sf| sf.code == 'u'}.each do |url|
-            acc << url.value
-          end
-        when '2'
-          # do nothing
-        else
-          z3 = [f['z'], f['3']].join(' ')
-          unless notfulltext.match(z3)
-            acc << f['u'] unless f['u'].nil?
-          end
-        end
-      end
-    end
+    # notfulltext = /abstract|description|sample text|table of contents|/i
 
-    # Very similar to url_fulltext_display. Should DRY up.
-    to_field 'url_suppl_display' do |rec, acc|
-      rec.fields('856').each do |f|
-        case f.indicator2
-        when '2'
-          f.find_all{|sf| sf.code == 'u'}.each do |url|
-            acc << url.value
-          end
-        when '0'
-          # do nothing
-        else
-          z3 = [f['z'], f['3']].join(' ')
-          if notfulltext.match(z3)
-            acc << f['u'] unless f['u'].nil?
-          end
-        end
-      end
-    end
+
   end
 end
